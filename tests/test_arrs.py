ba4f6ea0f3c999e6e484e0a225145fb5aca39907
Mihail <mihail.bashkatov@gmail.com>
2023-07-21 07:20:27 +0200

    Added 7 asserts for test_slice. Current test coverage is 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,10 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4], -5, 3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -10, 5) == [1, 2, 3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -3) == [5, 6, 7]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -5) == [3, 4, 5,  6, 7]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -5, 4) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -5, -2) == [3, 4, 5]
