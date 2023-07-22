16ddb4625c93a89d029a0ed1a114152963d2c26d
Mihail <mihail.bashkatov@gmail.com>
2023-07-22 20:45:52 +0200

    Added fixtures to test_arrs

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,3 +1,6 @@
+import pytest
+
+from tests.conftest import generate_list
 from utils import arrs
 
 
@@ -6,13 +9,13 @@ def test_get():
     assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(generate_list):
+    assert arrs.my_slice(generate_list, 1, 3) == [2, 3]
+    assert arrs.my_slice(generate_list, 1) == [2, 3, 4, 5, 6, 7]
     assert arrs.my_slice([]) == []
-    assert arrs.my_slice([1, 2, 3, 4], -5, 3) == [1, 2, 3]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -10, 5) == [1, 2, 3, 4, 5]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -3) == [5, 6, 7]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -5) == [3, 4, 5,  6, 7]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -5, 4) == [3, 4]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -5, -2) == [3, 4, 5]
+    assert arrs.my_slice(generate_list, -5, 3) == [3]
+    assert arrs.my_slice(generate_list, -10, 5) == [1, 2, 3, 4, 5]
+    assert arrs.my_slice(generate_list, -3) == [5, 6, 7]
+    assert arrs.my_slice(generate_list, -5) == [3, 4, 5,  6, 7]
+    assert arrs.my_slice(generate_list, -5, 4) == [3, 4]
+    assert arrs.my_slice(generate_list, -5, -2) == [3, 4, 5]
